e89c4ccabf413dba4911f15e88c37595acc9e18f
Coolbilal <109246528+Coolbilal@users.noreply.github.com>
2026-06-08 13:14:05 +0300

    fix: supabase generic tipi any yapıldı
    fix: supabase generic tipi any yapıldı

--- admin/src/lib/supabase.ts
+++ admin/src/lib/supabase.ts
@@ -4,7 +4,7 @@ import type { Database } from './types';
 export const SUPABASE_URL = 'https://cvfoufneshgqebynbxsc.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2Zm91Zm5lc2hncWVieW5ieHNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0MTQ1MzgsImV4cCI6MjA5NTk5MDUzOH0.taTlHNXBJLzH2UFUKBckgZu4YfYbYtp6fBzXjFZOwsw';
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
+export const supabase = createClient<any>(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true },
 });
 
